7ce4ebaf68b5f402842ff68eebec14ea4475a78f
Arindam-Harh <arindamharh96@gmail.com>
2026-06-03 03:16:21 +0530

    Adding API, and displaying dynamic data.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Modal } from "../components/Modal.jsx";
-// const API_KEY = `bfd3d0b5ded135b477ad7c473a4ae359`;
-// const BASE_URL = `https://api.openweathermap.org/data/2.5/weather`;
 
-// const url = `${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`;
+const API_KEY = `bfd3d0b5ded135b477ad7c473a4ae359`;
+const BASE_URL = `https://api.openweathermap.org/data/2.5/weather`;
+
 export const App = () => {
+  const [date, setDate] = useState(new Date());
   const [city, setCity] = useState("");
   const [showModal, setShowModal] = useState(false);
+  const [geoWeather, setGeoWeather] = useState([]);
+  const [weather, setWeather] = useState(null);
+
+  const displayCity = async (city) => {
+    try {
+      const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`;
+
+      const geoRes = await fetch(geoUrl);
+      const geoData = await geoRes.json();
+      console.log(geoData);
+      if (geoData.length == 0) {
+        setShowModal(true);
+      }
+      const lat = geoData[0].lat;
+      const lon = geoData[0].lon;
+
+      const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+      setGeoWeather(geoData);
+
+      const res = await fetch(weatherUrl);
+      const info = await res.json();
+      console.log(info);
+      setWeather(info);
+    } catch (error) {
+      setShowModal(true);
+      console.log(error);
+    }
+  };
 
   const getCityName = (e) => {
     e.preventDefault();
@@ -16,8 +45,21 @@ export const App = () => {
       return;
     }
     console.log(city);
+    displayCity(city);
     setCity("");
   };
+  useEffect(() => {
+    displayCity("Kolkata");
+  }, []);
+
+  useEffect(() => {
+  const interval = setInterval(() => {
+    setDate(new Date());
+  }, 1000);
+
+  return () => clearInterval(interval);
+}, []);
+
   return (
     <>
       <div className="main">
@@ -44,15 +86,16 @@ export const App = () => {
             </button>
           </form>
           <div className="temp">
-            <h2 className="temperature">temperature</h2>
-            <h2 className="city">City Name</h2>
-            <h2 className="date">date</h2>
+            <h2 className="temperature">{weather?.main?.temp} &deg;C</h2>
+            <h2 className="city">{geoWeather[0]?.name}</h2>
+            <h2 className="date">{date.toLocaleTimeString()}</h2>
+            <h2 className="date">{date.toLocaleDateString()}</h2>
           </div>
           <div className="weather_data">
-            <section className="wdata wind_speed">wind</section>
-            <section className="wdata feels_like">feels like</section>
-            <section className="wdata humidity">humidity</section>
-            <section className="wdata preception">preception</section>
+            <section className="wdata wind_speed">{weather?.wind?.speed}  m/s</section>
+            <section className="wdata feels_like">{weather?.main?.feels_like} &deg;C</section>
+            <section className="wdata humidity">{weather?.main?.humidity} %</section>
+            <section className="wdata preception">{weather?.main?.pressure} hPa</section>
           </div>
         </section>
       </div>
