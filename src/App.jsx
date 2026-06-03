a3bf2cf11c2ae50837d1332f6ad62022442a37ad
Arindam-Harh <arindamharh96@gmail.com>
2026-06-03 11:43:03 +0530

    Almost done.

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,74 @@ import { FaTemperatureHigh } from "react-icons/fa";
 import { FaTachometerAlt } from "react-icons/fa";
 import { FaRegClock } from "react-icons/fa";
 import { CiCalendarDate } from "react-icons/ci";
+import { WiStrongWind } from "react-icons/wi";
+import {
+  WiDaySunny,
+  WiCloud,
+  WiCloudy,
+  WiFog,
+  WiSmoke,
+  WiDust,
+  WiRain,
+  WiShowers,
+  WiThunderstorm,
+  WiSnow,
+  WiSleet,
+  WiTornado,
+  WiDayCloudy,
+} from "react-icons/wi";
 
+const weatherIconMap = {
+  "clear sky": WiDaySunny,
+
+  "few clouds": WiDayCloudy,
+  "scattered clouds": WiCloud,
+  "broken clouds": WiCloudy,
+  "overcast clouds": WiCloudy,
+
+  mist: WiFog,
+  fog: WiFog,
+  haze: WiSmoke,
+  smoke: WiSmoke,
+  dust: WiDust,
+  sand: WiDust,
+  ash: WiDust,
+  squalls: WiStrongWind,
+  tornado: WiTornado,
+
+  "light rain": WiRain,
+  "moderate rain": WiRain,
+  "heavy intensity rain": WiRain,
+  "very heavy rain": WiRain,
+  "extreme rain": WiRain,
+  "freezing rain": WiSleet,
+  "light intensity shower rain": WiShowers,
+  "shower rain": WiShowers,
+  "heavy shower rain": WiShowers,
+
+  "thunderstorm with light rain": WiThunderstorm,
+  "thunderstorm with rain": WiThunderstorm,
+  "thunderstorm with heavy rain": WiThunderstorm,
+  "light thunderstorm": WiThunderstorm,
+  thunderstorm: WiThunderstorm,
+  "heavy thunderstorm": WiThunderstorm,
+  "ragged thunderstorm": WiThunderstorm,
+  "thunderstorm with light drizzle": WiThunderstorm,
+  "thunderstorm with drizzle": WiThunderstorm,
+  "thunderstorm with heavy drizzle": WiThunderstorm,
+
+  "light snow": WiSnow,
+  snow: WiSnow,
+  "heavy snow": WiSnow,
+  sleet: WiSleet,
+  "light shower sleet": WiSleet,
+  "shower sleet": WiSleet,
+  "light rain and snow": WiSleet,
+  "rain and snow": WiSleet,
+  "light shower snow": WiSnow,
+  "shower snow": WiSnow,
+  "heavy shower snow": WiSnow,
+};
 const API_KEY = `bfd3d0b5ded135b477ad7c473a4ae359`;
 const BASE_URL = `https://api.openweathermap.org/data/2.5/weather`;
 
@@ -59,18 +126,22 @@ export const App = () => {
   }, []);
 
   useEffect(() => {
-  const interval = setInterval(() => {
-    setDate(new Date());
-  }, 1000);
+    const interval = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
 
-  return () => clearInterval(interval);
-}, []);
+    return () => clearInterval(interval);
+  }, []);
+  const getIcon = (type) => {
+    return weatherIconMap[type] || WiDaySunny;
+  };
+  const Icon = getIcon(weather?.weather[0]?.description);
 
   return (
     <>
       <div className="main">
         <header className="h">
-          <h1>Weather in Your City</h1>
+          <h1>Check Weather</h1>
         </header>
         <section className="body">
           {showModal && (
@@ -91,17 +162,37 @@ export const App = () => {
               Search
             </button>
           </form>
-          <div className="temp">
-            <h2 className="temperature">{weather?.main?.temp} &deg;C</h2>
+          <div className="temp fade-in">
+            <h2 className="temperature fade-in">{weather?.main?.temp} °C</h2>
             <h2 className="city">{geoWeather[0]?.name}</h2>
-            <h2 className="date">{date.toLocaleTimeString()}</h2>
+            <h2 className="description">
+              {weather?.weather[0]?.description
+                  .toLowerCase()
+                  .split(" ")
+                  .map((w) => w[0].toUpperCase() + w.slice(1))
+                  .join(" ")}
+            </h2>
+            <h2 className="time">{date.toLocaleTimeString()}</h2>
+            <h2 className="wicon">{Icon && <Icon size={30} />}</h2>
             <h2 className="date">{date.toLocaleDateString()}</h2>
           </div>
-          <div className="weather_data">
-            <section className="wdata wind_speed"><FaWind size={25}/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{weather?.wind?.speed}  m/s</section>
-            <section className="wdata feels_like"><FaTemperatureHigh size={25}/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{weather?.main?.feels_like} &deg;C</section>
-            <section className="wdata humidity"><WiHumidity size={25}/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{weather?.main?.humidity} %</section>
-            <section className="wdata preception"><FaTachometerAlt size={25}/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{weather?.main?.pressure} hPa</section>
+          <div className="weather_data fade-in">
+            <section className="wdata wind_speed">
+              <FaWind size={25} /> &nbsp;&nbsp;&nbsp;
+              {weather?.wind?.speed} m/s
+            </section>
+            <section className="wdata feels_like">
+              <FaTemperatureHigh size={25} /> &nbsp;&nbsp;
+              {weather?.main?.feels_like} &deg;C
+            </section>
+            <section className="wdata humidity">
+              <WiHumidity size={25}/> &nbsp;&nbsp;&nbsp;
+              {weather?.main?.humidity} %
+            </section>
+            <section className="wdata preception">
+              <FaTachometerAlt size={25} /> &nbsp;&nbsp;&nbsp;
+              {weather?.main?.pressure} hPa
+            </section>
           </div>
         </section>
       </div>
